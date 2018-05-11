045f865cac087ca7323ad3567cd8fa0942643edf
Olgethorpe <dondusko@fordham.edu>
2018-05-10 22:42:41 -0400

    Capabilities to pull all the data that is assumed to be needed.

--- pull_data.sh
+++ pull_data.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 # Constant file names to reference for pulling data
-JSON_FILE_NAMES=(getmarkets)
-TEXT_FILE_NAMES=(marketName)
+JSON_FILE_NAMES=(getmarketsummaries)
+TEXT_FILE_NAMES=(Ask BaseVolume Bid High Last Low MarketName OpenBuyOrders OpenSellOrders Volume)
 
 # Function for pulling json data to be cleaned
 pull_data() {
   for name in $JSON_FILE_NAMES
     do
-      if ! [ -e "$name.json" ]
-        then
-        wget -qO- https://bittrex.com/api/v1.1/public/$name | jq ".result" > "$name.json"
-      elif ! [ wget -qO- https://bittrex.com/api/v1.1/public/$name | jq ".result | length" = ]
-      else
-        echo $(wget -qO- https://bittrex.com/api/v1.1/public/$name | jq ".result | length")
-      fi
+      wget -qO- https://bittrex.com/api/v1.1/public/$name | jq ".result" > "$name.json"
     done
 }
 
 # Function for cleaning and parsing to store in text files to use later
 clean_data() {
-  for ((i=0;i<${#JSON_FILE_NAMES[@]};i++))
+  for ((i=0;i<${#TEXT_FILE_NAMES[@]};i++))
     do
-      if ! [ -e "${TEXT_FILE_NAMES[$i]}s.txt" ] ; then
-        echo "${TEXT_FILE_NAMES[$i]}s.txt Not found"
-      fi
+      for value in $(jq ".[].${TEXT_FILE_NAMES[$i]}" ${JSON_FILE_NAMES[0]}.json)
+        do
+          echo ${value//\"/} >> "${TEXT_FILE_NAMES[$i]}.txt"
+        done
     done
 }
 
 # Main body of script starts here
   pull_data
   clean_data
+  
+  # Aggregate compilation of data in the order of TEXT_FILE_NAMES
+  paste -d "," *.txt > aggregate.txt
